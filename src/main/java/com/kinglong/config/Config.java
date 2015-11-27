6e733e9c85bcdcc90c3cdc39aa62d809c7833f5a
jinlong.chen <jinlong.chen@tqmall.com>
2015-11-27 17:26:21 +0800

    添加配置文件方式

--- src/main/java/com/kinglong/config/Config.java
+++ src/main/java/com/kinglong/config/Config.java
@@ -1,6 +1,8 @@
 package com.kinglong.config;
 
-import java.sql.Connection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Created by chenjinlong on 15/6/5.
@@ -24,75 +26,65 @@ public class Config {
 
     public final static String TYPE_BLOB = "blob";
 
+    public static String TABLE;
+
+    public static String BASE_PATH;
+
+    public static String BASE_PACKAGE;
+
+    public static String  MODULE_NAME = "auto"; // 对应模块名称（根据自己模块做相应调整!!!务必修改^_^）
+
+    public static Integer RTN_TYPE;
+
+    public static String DB_PROPERTIES;
+
+    public static String IBATISTOOL_PROPERTIES;
+
+    public static String APPLICATION_PROPERTIES = "application.properties";
 
 
+    static {
 
+        Properties appProperties = new Properties();
+        InputStream appIn = ClassLoader.getSystemResourceAsStream(APPLICATION_PROPERTIES);
+        try {
+            appProperties.load(appIn);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        IBATISTOOL_PROPERTIES = appProperties.getProperty("ibatistool.properties");
+        DB_PROPERTIES = appProperties.getProperty("jdbc.properties");
 
-    public final static String TABLE = "db_tire_goods";
+        Properties itProperties = new Properties();
+        InputStream itIn = ClassLoader.getSystemResourceAsStream(IBATISTOOL_PROPERTIES);
 
-    public final static String
-//            BASE_PATH = "/Users/chenjinlong/ideaProjects/centimeter/src/dal/domain/";
-//            BASE_PATH = "/Users/chenjinlong/ideaProjects/centimeter/src/dal/domain/";
-//            BASE_PATH = "/Users/chenjinlong/ideaProjects/tqmallstall/tqmallstall-dal/src/main/java/com/tqmall/tqmallstall/";
-            BASE_PATH = "/Users/chenjinlong/IdeaProjects/wind/wind-dal/src/main/java/com/tqmall/wind/";
-//            BASE_PATH = "/Users/chenjinlong/IdeaProjects/tqmall/shop/dal/src/main/java/com/tqmall/shop/";
-//            BASE_PATH = "/Users/chenjinlong/IdeaProjects/kinglong/maze/dal/src/main/java/com/kinglong/maze/dal/";
+        try {
+            itProperties.load(itIn);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
 
+        //普通参数的初始化
+        RTN_TYPE = Integer.parseInt(itProperties.getProperty("xml_ret_type"));
+        BASE_PACKAGE = itProperties.getProperty("base_package");
+        BASE_PATH = itProperties.getProperty("base_path");
+        TABLE = itProperties.getProperty("table");
+    }
 
-    public final static String BASE_PACKAGE = "com.tqmall.wind";
-//    public final static String BASE_PACKAGE = "com.tqmall.tqmallstall";
-//    public final static String BASE_PACKAGE = "com.kinglong.maze.dal";
+    public static String BEAN_PATH = BASE_PATH+MODULE_NAME+"/entity";
 
-//    public final static String  MODULE_NAME = "dev_autoparts"; // 对应模块名称（根据自己模块做相应调整!!!务必修改^_^）
-//    public final static String  MODULE_NAME = "qa_autoparts"; // 对应模块名称（根据自己模块做相应调整!!!务必修改^_^）
-    public final static String  MODULE_NAME = "dev_wind"; // 对应模块名称（根据自己模块做相应调整!!!务必修改^_^）
-//    public final static String  MODULE_NAME = "dev_alidb"; // 对应模块名称（根据自己模块做相应调整!!!务必修改^_^）
+    public static String MAPPER_PATH = BASE_PATH+MODULE_NAME+"/mapper";
 
-    public final static String BEAN_PATH = BASE_PATH+MODULE_NAME+"/entity";
+    public static String XML_PATH = BASE_PATH+MODULE_NAME+"/xml";
 
-    public final static String MAPPER_PATH = BASE_PATH+MODULE_NAME+"/mapper";
+    public static String BEAN_PARAM_PATH = BASE_PATH+MODULE_NAME+"/param";
 
-    public final static String XML_PATH = BASE_PATH+MODULE_NAME+"/xml";
+    public static String BEAN_PACKAGE = BASE_PACKAGE+"." + MODULE_NAME + ".entity";
 
-    public final static String BEAN_PARAM_PATH = BASE_PATH+MODULE_NAME+"/param";
+    public static String BEAN_PARAM_PACKAGE = BASE_PACKAGE+"."+MODULE_NAME+".param";
 
-    public final static String BEAN_PACKAGE = BASE_PACKAGE+"." + MODULE_NAME + ".entity";
+    public static String MAPPER_PACKAGE = BASE_PACKAGE+"." + MODULE_NAME + ".mapper";
 
-    public final static String BEAN_PARAM_PACKAGE = BASE_PACKAGE+"."+MODULE_NAME+".param";
-
-    public final static String MAPPER_PACKAGE = BASE_PACKAGE+"." + MODULE_NAME + ".mapper";
-
-    //tqmallstall
-//    public final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
-//
-//    public final static String USER = "shop_T_manager";
-//
-//    public final static String PASSWORD = "AI*k#$xv!Kmw";
-
-    //wind
-    public final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
-
-    public final static String USER = "wind_S_shop";
-
-    public final static String PASSWORD = "s3Db2WcakBaoEKwb";
-
-//    public final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
-//
-//    public final static String USER = "admin";
-//
-//    public final static String PASSWORD = "max6and7";
-
-    //tqmallstall
-    public final static String URL = "jdbc:mysql://121.199.28.244:3306/" + MODULE_NAME + "?characterEncoding=UTF-8" ;
-
-//    public final static String URL = "jdbc:mysql://120.26.56.174:3306/" + MODULE_NAME + "?characterEncoding=UTF-8" ;
-
-    //wind
-//    public final static String URL = "jdbc:mysql://121.199.28.244:3306/dev_wind?characterEncoding=UTF-8" ;
-
-    public static final Integer RTN_TYPE = 0;
-    //+ "&amp;zeroDateTimeBehavior=convertToNull";
-
-
-//    public final static String URL = "jdbc:mysql://192.168.1.253:3306/" + MODULE_NAME + "?characterEncoding=utf8";
 }
--- src/main/java/com/kinglong/db/Conn.java
+++ src/main/java/com/kinglong/db/Conn.java
@@ -2,9 +2,12 @@ package com.kinglong.db;
 
 import com.kinglong.config.Config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by chenjinlong on 15/6/5.
@@ -33,8 +36,35 @@ public class Conn {
     }
 
     private void init() throws ClassNotFoundException, SQLException {
-        Class.forName(Config.DRIVER_NAME);
-        conn = DriverManager.getConnection(Config.URL, Config.USER, Config.PASSWORD);
+
+        Properties jdbcProperties = new Properties();
+        //Config.DB_PROPERTIES
+        InputStream jdbcIn = ClassLoader.getSystemResourceAsStream(Config.DB_PROPERTIES);
+        try {
+            jdbcProperties.load(jdbcIn);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        String db_name = jdbcProperties.getProperty("db_name");
+        //数据库的参数初始化
+        String driver_name  = jdbcProperties.getProperty("driver");
+        if (driver_name == null) {
+            driver_name = "com.mysql.jdbc.Driver";
+        }
+        String user = jdbcProperties.getProperty("user");
+        String password = jdbcProperties.getProperty("password");
+        String db_url = jdbcProperties.getProperty("db_url");
+
+        String db_charset = jdbcProperties.getProperty("db_charset");
+        if (db_charset == null) {
+            db_charset = "UTF-8";
+        }
+        String url = String.format("%s%s?characterEncoding=%s",db_url,db_name,db_charset);
+
+
+        Class.forName(driver_name);
+        conn = DriverManager.getConnection(url, user, password);
     }
 
 
